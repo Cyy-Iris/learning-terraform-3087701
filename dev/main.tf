78f66564cc737a9d6442413b8871db69d45d9dfc
Cyy-Iris <82180480+Cyy-Iris@users.noreply.github.com>
2023-03-06 16:07:30 +0100

    modif env

--- dev/main.tf
+++ dev/main.tf
@@ -1,3 +1,10 @@
 module "dev" {
     source = "../modules/blog"
+    environment = {
+        name = "dev"
+        network_prefix = "10.1"
+    }
+
+    asg_min_size = 1
+    asg_max_size = 1
 }
--- qa/main.tf
+++ qa/main.tf
@@ -1,4 +1,10 @@
 module "qa" {
-    
+    environment = {
+        name = "qa"
+        network_prefix = "10.1"
+    }
+
+    asg_min_size = 1
+    asg_max_size = 1
     source = "../modules/blog"
 }
